db0a18c3a307c48592da735df987185d98c8fa8a
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-19 09:58:03 +0000

    f

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-    char ch = 'a';
+	char ch = 'a';
 
-    while (ch <= 'z')
-    {
-        putchar(ch);
-        ch++;
-    }
-    putchar('\n');
-
-    return (0);
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
