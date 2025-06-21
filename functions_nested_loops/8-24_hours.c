cfeadc03ed8a129953bf4db690e4d3e5555432af
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-21 07:10:39 +0000

    Add jack_bauer function to print every minute of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Description: Prints time in HH:MM format from 00:00 to 23:59
+ * using nested loops and _putchar.
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
+}
