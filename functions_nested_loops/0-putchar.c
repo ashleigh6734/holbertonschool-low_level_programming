c1e4e1bafbc20ced51668061de07281165e3898f
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-21 03:18:42 +0000

    m

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * description
+ * return: 0
+ */
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
