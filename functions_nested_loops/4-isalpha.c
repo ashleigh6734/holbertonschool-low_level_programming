0eaf07f92cf5315a4c244f999668ddba636cace4
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-21 06:44:54 +0000

    Add _islower function and update header

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is an alphabetic letter
+ * @c: The character to check (ASCII value)
+ *
+ * Description: Returns 1 if c is a letter, lowercase or uppercase,
+ * otherwise returns 0.
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
