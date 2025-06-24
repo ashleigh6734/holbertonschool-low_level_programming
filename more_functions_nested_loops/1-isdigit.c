180b0f6e985bc63d18860e8732adcc25cefcd9f0
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-24 04:23:20 +0000

    fileadd

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description - checking for a digit
+ * Return: 0 always success
+ */
+
+int _isupper(int c)
+{
+	return (c >= '0' && c <= '9');
+}
