4e8877b5ee2ec8c6202d06862f76777dfb1f2070
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-28 02:15:11 +0000

    printrev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- * description - reverse string
- * Return: 0
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: the string to print
  */
-
 void print_rev(char *s)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[1] != '\0')
-		i--;
+    /* First, find the length of the string */
+    while (s[i] != '\0')
+        i--;
 
-	while (i >= 0)
-	{
-		_putchar(s[1]);
-		i--;
-	}
-	_putchar('\n');
+    /* Then print characters in reverse order */
+    while (i >= 0)
+    {
+        _putchar(s[i]);
+        i--;
+    }
+
+    _putchar('\n');
 }
