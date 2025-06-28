6ef117801dc3517496d0a3e519ae582e649ceec1
ashleigh6734 <ashleigh6734@gmail.com>
2025-06-28 02:25:48 +0000

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - reverses a string
+ * @s: string to be reversed, provided by main
+ * Return - void
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int element;
+
+	/* find array length */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (element = (length - 1); element >= 0; element--);
+}
