23220895981a07b6cbe312af7fc248710923109d
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-05 03:00:26 +0000

    printrev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion
+ *
+ * @s: string to print out
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_print_rev_recursion(&s[(i + 1)]);
+		_putchar(s[i]);
+	}
+}
