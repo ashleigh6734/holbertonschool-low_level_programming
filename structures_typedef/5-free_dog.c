3d76aa822f8dac271116b0495cd43845cc2f9e6d
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-13 04:57:12 +0000

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a dog
+ * @d: pointer to the dog to free
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
