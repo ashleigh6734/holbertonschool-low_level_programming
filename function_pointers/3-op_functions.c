595dff0bf3bcfdc8927b290314fe46b5802877e7
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-16 11:35:19 +0000

    bulk add

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
