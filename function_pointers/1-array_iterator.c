ae91c0781744af01956eef8f7a351f3fc5692905
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-16 10:34:12 +0000

    arrayiterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	for (size_t i = 0; i < size; i++)
+		action(array[i]);
+}
