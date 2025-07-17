a010d224c54185556bfc2907cc5cbf669b2247d5
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-17 09:38:32 +0000

    printers

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,63 +1,35 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "3-get_print_func.c"
+#include "3-print_functions.c"
 
 /**
- * print_all - prints anything based on format string
- * @format: list of argument types
+ * print_all - print all the arguments provided
+ * @format: list of the types of the arguments passed to the function
+ *
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *str;
-    char *sep;
-    char format_char;
-    int int_arg;
-    float float_arg;
-    char char_arg;
+	int i = 0;
+	va_list args;
+	void (*f)(va_list);
 
-    va_start(args, format);
-    sep = "";
-
-    while (format != NULL && format[i] != '\0')
-    {
-        format_char = format[i];
-
-        if (format_char == 'c' || format_char == 'i' || format_char == 'f' || format_char == 's')
-        {
-            printf("%s", sep);
-
-            if (format_char == 'c')
-            {
-                char_arg = va_arg(args, int);
-                printf("%c", char_arg);
-            }
-
-            if (format_char == 'i')
-            {
-                int_arg = va_arg(args, int);
-                printf("%d", int_arg);
-            }
-
-            if (format_char == 'f')
-            {
-                float_arg = (float)va_arg(args, double);
-                printf("%f", float_arg);
-            }
-
-            if (format_char == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s", str);
-            }
-
-            sep = ", ";
-        }
-
-        i++;
-    }
-
-    va_end(args);
-    printf("\n");
+	va_start(args, format);
+	/* iterate through format string */
+	while (format != NULL && format[i] != '\0')
+	{
+		/* call get_print_func for each char in the format string */
+		f = get_print_func(format[i]);
+		if (f != NULL)
+		{
+			f(args);
+			if (format[i + 1] != '\0')
+				printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,19 @@
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
+#include <stdarg.h>
 
+typedef struct printer
+{
+	char *type;
+	void (*f)(va_list args);
+} printer_t;
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_string(va_list args);
+void print_float(va_list args);
+void (*get_print_func(char type))(va_list);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
