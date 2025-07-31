a500770a8eb058939e2e5de744abfffce7db5f22
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-31 05:14:34 +0000

    task1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
+	const dlistint_t *current_node = h;
 
+	while (current_node != NULL)
+	{
+		printf("%d\n", current_node->n);
+		count++;
+		current_node = current_node->next;
+	}
 	return (count);
 }
