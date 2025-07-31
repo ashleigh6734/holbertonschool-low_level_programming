b667c2a835630fef244415c1e3abe88f51f8d1ea
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-31 04:04:31 +0000

    task 5

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees memory taken by a linked list
+ * @head: head of list to free
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
