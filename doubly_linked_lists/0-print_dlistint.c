580da9caccab7ede37c6b4cca13e1497b964b3c2
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-31 05:07:20 +0000

    task1

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dlistint - print all items in a doubly linked list
+ * @h: pointer to head of list
+ * Return: number of nodes in list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
