b1574edf5a274bd008bba2e223a3ea57324a6257
ashleigh6734 <ashleigh6734@gmail.com>
2025-08-02 03:16:45 +0000

    task2

---
+++ file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - create a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd; /* file descriptor */
+	ssize_t bytes = 0; /* number of bytes read/written */
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		bytes = 0;
+	else /* get length of string */
+	{
+		while (text_content[bytes] != '\0')
+		{
+			bytes++;
+		}
+	}
+
+	bytes = write(fd, text_content, bytes);
+	if (bytes == -1)
+		return (-1);
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
